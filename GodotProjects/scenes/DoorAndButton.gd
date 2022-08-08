82b9a6c5eeaea0cea95866836d549fd3b9b274ee
Erikim06 <erikim06@kingshigh.school.nz>
2022-08-08 14:59:07 +1200

    1

--- GodotProjects/scenes/DoorAndButton.gd
+++ GodotProjects/scenes/DoorAndButton.gd
@@ -6,6 +6,6 @@ func _ready():
 	
 
 func _on_Area2D_body_entered(body):
-	if body. is_in_group("player"):
+	if body.is_in_group("player"):
 		$AnimationPlayer.play("ButtonDown")
 		$AnimationPlayer.play("DoorOpen")
