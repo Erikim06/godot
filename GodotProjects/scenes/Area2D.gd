9cfad5197ded060188de82e8a1e9aed86560fe48
Erikim06 <erikim06@kingshigh.school.nz>
2022-08-19 13:55:29 +1200

    1

--- GodotProjects/scenes/Area2D.gd
+++ GodotProjects/scenes/Area2D.gd
@@ -4,8 +4,4 @@ func _ready():
 	$AnimationPlayer.play("SpikeTrigger")
 	
 
-func _on_Area2D_body_entered(body):
-	if body.is_in_group("player"):
-		print("player dead")
-		if GameStats.check_reset() == false:
-			body.global_position=GameStats.get_spawn().global_position
+
